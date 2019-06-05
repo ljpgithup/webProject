82090cb412b909c721ce572e7c9b364477c9b628
liu <1782440858@qq.com>
2019-06-05 14:25:46 +0800

    第一次提交manager

---
+++ wl-itmuch-manager/wl-itmuch-manager-web/src/main/java/com/osyunge/controller/ItemController.java
@@ -0,0 +1,22 @@
+package com.osyunge.controller;
+
+import com.osyunge.pojo.TbItem;
+import com.osyunge.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("/item/{id}")
+    @ResponseBody
+    private TbItem findItemById(@PathVariable Long id){
+        TbItem item = itemService.findItemById(id);
+        return item;
+    }
+}
